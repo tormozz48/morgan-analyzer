714333fb84c7d91a06e28d59961a39e0e4d89aa3
tormozz48 <tormozz48@gmail.com>
2018-04-13 21:43:22 +0300

    Add stripping ANSI characters. Provide optimization

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const _ = require('lodash');
 const hl = require('highland');
 
+const ANSI_REGEXP = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
 const HTTP_METHODS = ['GET', 'POST', 'OPTIONS', 'POST', 'PUT', 'PATCH', 'DELETE'];
 const PERCENTILES = [0.35, 0.50, 0.80, 0.95, 0.99];
 
 const isMorganRequestRecord = (row) => HTTP_METHODS.some((method) => row.indexOf(method) > -1);
+
+const removeANSI = (str) => str.replace(ANSI_REGEXP, '');
 
 const splitRowBySpaces = (row) => _.split(row, ' ');
 
 const timeComparator = (a, b) => a.time - b.time;
 
 const toRequestRecord = (rowChunks) => {
-    const [,,method, url, status, time, measure, ...other] = rowChunks;
+    const [, , method, url, status, time, measure] = rowChunks;
     return {
         method,
         url,
@@ -19,7 +22,7 @@ const toRequestRecord = (rowChunks) => {
         time: Number(time),
         measure
     };
-}
+};
 
 const calculatePercentiles = (res) => {
     const total = res.length;
@@ -34,7 +37,7 @@ const displayPercentiles = (percentiles) => {
         .toPairs()
         .map((item) => `${100 * Number(item[0])}-th: ${item[1]} ms`)
         .join('\n');
-}
+};
 
 const displayRequestRecord = (record) =>{
     return `Time: ${record.time} ${record.measure} Url: ${record.method} ${record.url}\n`;
@@ -43,17 +46,13 @@ const displayRequestRecord = (record) =>{
 const stream = hl(process.stdin)
     .split()
     .filter(isMorganRequestRecord)
-    .map(splitRowBySpaces)
-    .map(_.compact)
-    .map(toRequestRecord)
+    .map(hl.compose(toRequestRecord, _.compact, splitRowBySpaces, removeANSI));
 
 stream
     .fork()
     .sortBy(timeComparator)
     .pluck('time')
-    .toArray(function(res) {
-        console.info(displayPercentiles(calculatePercentiles(res)));
-    });
+    .toArray(hl.compose(console.info, displayPercentiles, calculatePercentiles));
 
 stream
     .fork()
